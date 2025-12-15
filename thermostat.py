086affb39da66a079fbf7966bf5c06dd2dd1527e
tyler <tylermc94@gmail.com>
2025-12-14 19:49:14 -0500

    added thermostat.py to control the temperature settings.

---
+++ thermostat.py
@@ -0,0 +1,26 @@
+from machine import Pin, ADC, PWM
+import time
+
+#-----Variables-----
+mode = "heat"
+set_temp = 70
+current_temp = 68
+heater_state = "off"
+
+for _ in range(10):
+    # Simulate temperature change for testing
+    if heater_state == "on":
+        current_temp += 1
+    else:
+        current_temp -= 1
+
+    if set_temp < current_temp and mode == "heat":
+        print("Heating")
+        heater_state = "on"
+
+    if set_temp >= current_temp and mode == "heat":
+        print("Heater Off")
+        heater_state = "off"
+
+    print(f"Current Temp: {current_temp}, Heater: {heater_state}")
+    time.sleep(0.1)
